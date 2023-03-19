40e975b0449d318a104d3753f2811d5be962cb7c
Junbadatcoding <109511875+Junbadatcoding@users.noreply.github.com>
2023-03-18 21:28:18 -0400

    Update firebasetest1.js

--- exercise/firebasetest1.js
+++ exercise/firebasetest1.js
@@ -1,11 +1,4 @@
-const {
-    initializeApp,
-    getFirestore,
-    doc,
-    onSnapshot,
-    updateDoc,
-  } = window;
-  
+// Replace with your Firebase configuration object
 const firebaseConfig = {
     apiKey: "AIzaSyDPGDfIq5ykubrOY-ZtOcuUf51wjNzYoVM",
     authDomain: "juntestone-e6fd8.firebaseapp.com",
@@ -16,30 +9,27 @@ const firebaseConfig = {
     measurementId: "G-Z77NWRQGXJ"
   };
   
-// Initialize Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
-import { getFirestore, doc, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Get HTML elements
-const valueDisplay = document.getElementById("value-display");
-const inputValue = document.getElementById("input-value");
-const submitValueButton = document.getElementById("submit-value");
-
-// Listen for changes in Firestore
-const valueRef = doc(db, "values", "counter");
-onSnapshot(valueRef, (snapshot) => {
-  const data = snapshot.data();
-  valueDisplay.textContent = data.value;
-});
-
-// Update value in Firestore based on input box
-submitValueButton.addEventListener("click", async () => {
-  const newValue = inputValue.value;
-  if (newValue) {
-    await updateDoc(valueRef, { value: newValue });
-    inputValue.value = "";
-  }
-});
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore(app);
+  
+  // Get HTML elements
+  const valueDisplay = document.getElementById("value-display");
+  const inputValue = document.getElementById("input-value");
+  const submitValueButton = document.getElementById("submit-value");
+  
+  // Listen for changes in Firestore
+  const valueRef = db.collection("values").doc("counter");
+  valueRef.onSnapshot((snapshot) => {
+    const data = snapshot.data();
+    valueDisplay.textContent = data.value;
+  });
+  
+  // Update value in Firestore based on input box
+  submitValueButton.addEventListener("click", async () => {
+    const newValue = inputValue.value;
+    if (newValue) {
+      await valueRef.update({ value: newValue });
+      inputValue.value = "";
+    }
+  });
