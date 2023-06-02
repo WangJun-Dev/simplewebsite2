884b4d767f18c9bfa6bccabcdf8580772124e5a9
Junbadatcoding <109511875+Junbadatcoding@users.noreply.github.com>
2023-06-01 20:39:23 -0400

    Update ap.js

--- exercise/ap.js
+++ exercise/ap.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+let launches = [
+  {
+    id: 3276,
+    name: "Starlink-85 (2-10)",
+    missionDescription: "这是一个测试",
+    // ... other details
+  },
+  // ... other launches
+];
+
+app.get('/launches', (req, res) => {
+  res.json(launches);
+});
+
+app.get('/launches/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const launch = launches.find(launch => launch.id === id);
+  if (!launch) {
+    res.status(404).send('Launch not found');
+  } else {
+    res.json(launch);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
